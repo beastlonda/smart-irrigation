c85ee3b80cc3eb66aaa24f84b7f94285f78a1f5c
Divyanshu Kumawat <divyanshukumawat9170@gmail.com>
2026-03-10 23:36:17 +0530

    fix database connection for deployment

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,26 +2,22 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { DevicesModule } from './devices/devices.module';
-import { SensorReadingsModule } from './sensor-readings/sensor-readings.module';
 import { CropsModule } from './crops/crops.module';
+import { SensorReadingsModule } from './sensor-readings/sensor-readings.module';
 import { AutoActionsModule } from './auto-actions/auto-actions.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'smartfarm',
-      password: 'smartfarm123',
-      database: 'smartfarm_db',
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
       synchronize: true,
     }),
 
     DevicesModule,
-    SensorReadingsModule,
     CropsModule,
+    SensorReadingsModule,
     AutoActionsModule,
   ],
 })
